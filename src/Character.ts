e285e68ed65a1c223b1c0e30adbe5e20c4ec6f21
Zyrakia <mailzyrakia@gmail.com>
2021-12-18 20:26:15 -0800

    refactor: added docs to character and removed sync funcs

--- src/Character.ts
+++ src/Character.ts
@@ -1,42 +1,52 @@
-import promiseR15, { CharacterRigR15, CharacterRigR6, promiseR6 } from '@rbxts/promise-character';
+import promiseR15, { promiseR6 } from '@rbxts/promise-character';
 import { promiseChildOfClass } from '@rbxts/promise-child';
 
 export namespace Character {
+	/**
+	 * Returns the character model of the player.
+	 *
+	 * @param player The player to get the character model of.
+	 * @returns A promise that resolves with the character model of the player.
+	 */
 	export async function get(player: Player) {
 		return player.Character || Promise.fromEvent(player.CharacterAdded);
 	}
 
-	export function getSync(player: Player) {
-		return player.Character || player.CharacterAdded.Wait()[0];
-	}
-
+	/**
+	 * Promises for the R15 character rig of the player.
+	 *
+	 * @param player The player to get the R15 character rig of.
+	 * @returns A promise that resolves with the R15 character rig of the player.
+	 */
 	export async function getRig(player: Player) {
 		const char = await get(player);
 		return promiseR15(char);
 	}
 
-	export function getRigSync(player: Player) {
-		return getRig(player).await()[1] as CharacterRigR15;
-	}
-
+	/**
+	 * Promises for the R6 character rig of the player.
+	 *
+	 * @param player The player to get the R6 character rig of.
+	 * @returns A promise that resolves with the R6 character rig of the player.
+	 */
 	export async function getRigR6(player: Player) {
 		const char = await get(player);
 		return promiseR6(char);
 	}
 
-	export function getRigR6Sync(player: Player) {
-		return getRigR6(player).await()[1] as CharacterRigR6;
-	}
-
+	/**
+	 * Promises for either the R15 or the R6 character rig of the player,
+	 * depending on what type their Humanoid specifies.
+	 *
+	 * @param player The player to get the character rig of.
+	 * @returns A promise that resolves with the character rig of the player.
+	 * @throws If the specified rig type is not R15 or R6.
+	 */
 	export async function getRigEither(player: Player) {
 		const char = await get(player);
 		const type = (await promiseChildOfClass(char, 'Humanoid')).RigType.Name;
 		if (type === 'R6') return promiseR6(char);
 		if (type === 'R15') return promiseR15(char);
 		throw `Unhandled RigType: ${type} on ${char.Name}!`;
-	}
-
-	export function getRigEitherSync(player: Player) {
-		return getRigEither(player).await()[1] as CharacterRigR15 | CharacterRigR6;
 	}
 }
