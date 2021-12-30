b170c5c40488cd0fa65aeaa147c116ca491b2eab
Zyrakia <mailzyrakia@gmail.com>
2021-12-29 18:24:30 -0800

    feat: benchmark micros, benchmark.start/stop

--- src/Benchmark.ts
+++ src/Benchmark.ts
@@ -1,16 +1,30 @@
 import { RunService } from '@rbxts/services';
 
 class BenchmarkResult {
-	public constructor(private elapsed: number) {}
+	private micros: number;
+	private microsPerRun: number;
+
+	public constructor(elapsed: number, private runCount: number, private opName?: string) {
+		this.micros = elapsed * 1000000;
+		this.microsPerRun = this.micros / runCount;
+	}
 
 	/**
-	 * Writes the benchmark result to the console in the format:
-	 * Benchmark({opName}?): {elapsedTime}
+	 * Writes the average time of the operation in micros to the
+	 * console, followed with the total time in micros if the
+	 * operation was ran multiple times.
+	 *
+	 * Format:
+	 * Benchmark({opName}?): {runMicros} μs ({totalMicros?} μs)
 	 *
 	 * @param opName The name of the operation being benchmarked.
 	 */
-	public write(opName?: string) {
-		print(`Benchmark${opName ? `(${opName})` : ''}: ${this.elapsed}`);
+	public write(opName = this.opName) {
+		print(
+			`Benchmark${opName ? `(${opName})` : ''}: ${this.microsPerRun} μs${
+				this.runCount > 1 ? ` (${this.micros} μs)` : ''
+			}`,
+		);
 	}
 
 	/**
@@ -53,26 +67,74 @@ class BenchmarkResult {
 	/**
 	 * Returns the elapsed time of the benchmark.
 	 */
-	public getTime() {
-		return this.elapsed;
+	public getMicros() {
+		return this.micros;
+	}
+
+	/**
+	 * Returns the average time in micros.
+	 */
+	public getMicrosPerRun() {
+		return this.microsPerRun;
 	}
 }
 
 export namespace Benchmark {
+	const activeBenchmarks = new Map<string, number>();
+
 	/**
 	 * Runs a benchmark and returns a few utility functions to use the elapsed time.
 	 *
 	 * @param f The function to benchmark.
-	 * @param args The arguments to pass to the function.
+	 * @param runCount The number of times to run the function. Will be rounded and at least 1.
 	 *
 	 * @returns an object containing functions to use the elapsed time.
 	 */
-	export function run<A extends defined[]>(f: (...args: A) => void, ...args: A) {
-		const now = os.clock();
-		f(...args);
-		const elapsed = os.clock() - now;
+	export function run(f: () => void, runCount = 1) {
+		const runs = math.round(math.max(runCount, 1));
 
-		return new BenchmarkResult(elapsed);
+		const start = os.clock();
+		for (let i = 0; i < runs; i++) f();
+		const fin = os.clock();
+
+		return new BenchmarkResult(fin - start, runs);
+	}
+
+	/**
+	 * Starts a benchmark with the specified ID.
+	 * If a benchmark with the same ID is already running, this will throw.
+	 *
+	 * This benchmark can be stopped with {@link stop}, which
+	 * will return the benchmark result of time time between
+	 * the two calls.
+	 *
+	 * @param id The ID of the benchmark.
+	 */
+	export function start(id: string) {
+		if (activeBenchmarks.has(id)) throw `Benchmark with id(${id}) already started.`;
+		activeBenchmarks.set(id, os.clock());
+	}
+
+	/**
+	 * Used to stop a benchmark with the specified ID.
+	 * If no benchmark with the ID exists, this will throw.
+	 *
+	 * This will return the benchmark result of the time between
+	 * the two calls. If the opName in any of the write functions
+	 * is omitted, the ID of the benchmark will automatically be
+	 * used.
+	 *
+	 * @param id The ID of the benchmark.
+	 * @returns The benchmark result.
+	 */
+	export function stop(id: string) {
+		if (!activeBenchmarks.has(id)) throw `Benchmark with id(${id}) not started.`;
+		const start = activeBenchmarks.get(id)!;
+		const fin = os.clock();
+
+		activeBenchmarks.delete(id);
+
+		return new BenchmarkResult(fin - start, 1, id);
 	}
 
 	/**
@@ -80,9 +142,6 @@ export namespace Benchmark {
 	 *
 	 * @param opName The name of the profile.
 	 * @param f The function to profile.
-	 * @param args The arguments to pass to the function.
-	 *
-	 * @returns The return value of the function.
 	 */
 	export function runDebug<A extends defined[], R>(
 		opName: string,
