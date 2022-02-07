5173fa8cafecd0cf099cfcaee1c215ae55320c9b
Zyrakia <mailzyrakia@gmail.com>
2022-02-07 12:40:57 -0800

    feat: linking to new players

--- src/char-watcher/CharacterWatcher.ts
+++ src/char-watcher/CharacterWatcher.ts
@@ -2,6 +2,7 @@ import { Bin } from '@rbxts/bin';
 import { Ping } from '@rbxts/ping';
 
 export class CharacterWatcher {
+	private isDestroyed = false;
 	private bin = new Bin();
 
 	private addedPing = new Ping<[char: Model]>();
@@ -25,28 +26,57 @@ export class CharacterWatcher {
 	 * @param player The player to watch
 	 * @param existing Whether to fire for an existing character
 	 */
-	public constructor(private player: Player, private existing = true) {
-		this.bin.add(this.addedPing);
-		this.bin.add(this.removingPing);
-	}
+	public constructor(private player: Player, private existing = true) {}
 
 	/**
 	 * Start watching the player for character events, and
-	 * fire for existing characters if desired.
+	 * fire for the existing character if desired.
 	 */
 	public watch() {
-		if (this.bin.isEmpty()) throw 'CharacterWatcher is destroyed.';
+		this.linkTo(this.player);
+	}
 
-		if (this.existing) {
-			const char = this.player.Character;
-			if (char) this.added(char);
-		}
+	/**
+	 * Sets the player this watcher is watching,
+	 * this will clear all connections relating to
+	 * the previous player and create new ones for
+	 * the specified player.
+	 *
+	 * This will fire for existing characters if desired, there is
+	 * no need to call {@link watch} again, this will do the
+	 * same thing.
+	 *
+	 * This will do nothing if the player is already being watched.
+	 *
+	 * @param player The player to watch
+	 */
+	public setPlayer(player: Player) {
+		if (player === this.player) return;
+		this.bin.destroy();
+		this.linkTo(player);
+	}
 
-		const added = this.player.CharacterAdded.Connect((model) => this.added(model));
-		const removing = this.player.CharacterRemoving.Connect((model) => this.removing(model));
+	private linkTo(player: Player) {
+		if (!this.bin.isEmpty()) return;
+		if (this.isDestroyed) throw 'CharacterWatcher is destroyed.';
+		this.player = player;
 
+		const added = player.CharacterAdded.Connect((model) => this.added(model));
+		const removing = player.CharacterRemoving.Connect((model) => this.removing(model));
 		this.bin.add(added);
 		this.bin.add(removing);
+
+		if (this.existing) {
+			const char = player.Character;
+			if (char) this.added(char);
+		}
+	}
+
+	/**
+	 * Returns the player this watcher is currently watching.
+	 */
+	public getPlayer() {
+		return this.player;
 	}
 
 	/**
@@ -72,6 +102,10 @@ export class CharacterWatcher {
 	 * throw if asked to watch again.
 	 */
 	public destroy() {
+		if (this.isDestroyed) return;
+		this.isDestroyed = true;
 		this.bin.destroy();
+		this.addedPing.destroy();
+		this.removingPing.destroy();
 	}
 }
