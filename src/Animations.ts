51c2f54cfd6a0b60e58b4f0578e63dab619cc2ac
Zyrakia <mailzyrakia@gmail.com>
2022-02-09 12:28:46 -0800

    feat: animations resetAnimator

--- src/Animations.ts
+++ src/Animations.ts
@@ -5,10 +5,10 @@ export namespace Animations {
 	 * Extracts numbers from the specified animation or the animation
 	 * in the specified animation track and returns it parsed as a number.
 	 * This will not work for animations that have an animation ID property that
-     * includes any numbers except the actual asset ID, since all it's doing
-     * is extracting and parsing the numbers.
-     * 
-     * Example:
+	 * includes any numbers except the actual asset ID, since all it's doing
+	 * is extracting and parsing the numbers.
+	 *
+	 * Example:
 	 * ```ts
 	 * const animation = new Instance("Animation");
 	 * animation.AnimationId = "rbxassetid://1234"
@@ -18,15 +18,26 @@ export namespace Animations {
 	 * @param animation The animation to extract the number from.
 	 * @returns The number extracted from the animation.
 	 */
-	export function extractId(animation: Animation | AnimationTrack): number | undefined {
-		if (animation.IsA('Animation')) {
-			const stringId = Strings.extractNumbers(animation.AnimationId);
-			return tonumber(stringId);
-		}
-
-		const anim = animation.Animation;
+	export function extractId(animation: Animation | AnimationTrack) {
+		const anim = animation.IsA('Animation') ? animation : animation.Animation;
 		if (!anim) return;
 
-		return extractId(anim);
+		const stringId = Strings.extractNumbers(anim.AnimationId);
+		return tonumber(stringId);
+	}
+
+	/**
+	 * Iterates through all running tracks, stops each track, destroys it's animation and
+	 * then destroys the track itself.
+	 *
+	 * @param animator The animator to clear.
+	 */
+	export function resetAnimator(animator: Animator) {
+		const tracks = animator.GetPlayingAnimationTracks();
+		tracks.forEach((track) => {
+			track.Stop();
+			track.Animation?.Destroy();
+			track.Destroy();
+		});
 	}
 }
