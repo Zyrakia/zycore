4d87e46a651da8c70a9969e4c4ff951c07a8c55c
Zyrakia <mailzyrakia@gmail.com>
2022-02-24 00:17:50 -0800

    fix: cleanup

--- src/safety/Cleanup.ts
+++ src/safety/Cleanup.ts
@@ -2,21 +2,21 @@ export type CleanableValue =
 	| (() => unknown)
 	| { destroy(): void }
 	| { Destroy(): void }
+	| { disconnect(): void }
+	| { Disconnect(): void }
 	| { cancel(): void }
 	| { Cancel(): void }
-	| { disconnect(): void }
-	| { Disconnect(): void };
+	| CleanableValue[];
 
 export function cleanup<T extends CleanableValue>(value: T) {
-	if (typeIs(value, 'function')) value();
-	else if ('Disconnect' in value) value.Disconnect();
-	else if ('disconnect' in value) value.disconnect();
-	else if ('Destroy' in value) value.Destroy();
+	if (typeIs(value, 'RBXScriptConnection')) value.Disconnect();
+	else if (typeIs(value, 'Instance')) value.Destroy();
+	else if (typeIs(value, 'function')) value();
 	else if ('destroy' in value) value.destroy();
-	else if ('Cancel' in value) value.Cancel();
+	else if ('Destroy' in value) value.Destroy();
+	else if ('disconnect' in value) value.disconnect();
+	else if ('Disconnect' in value) value.Disconnect();
 	else if ('cancel' in value) value.cancel();
-}
-
-export function cleanupAll(...values: CleanableValue[]) {
-	for (const value of values) cleanup(value);
+	else if ('Cancel' in value) value.Cancel();
+	else value.forEach((v) => cleanup(v));
 }
